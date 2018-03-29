aaed71a594272b7fd28b60cc0d6dea59f4376a54
Alex Gumbin <lesha.gumbin@yandex.ru>
2018-03-29 17:21:56 +0300

    decrypt caesar added

--- homework1/caesar.py
+++ homework1/caesar.py
@@ -13,9 +13,29 @@ def inc_symbol(symbol):
     else:
         return symbol
 
+def dec_symbol(symbol):
+    ascii_number = ord(symbol)
+
+    if ((ascii_number >= 65 and ascii_number <= 90) or (ascii_number >= 97 and ascii_number <= 122)):
+        if (symbol == "c" or symbol == "C"):
+            return chr(ascii_number + 23)
+        elif (symbol == "b" or symbol == "B"):
+            return chr(ascii_number + 23)
+        elif (symbol == "a" or symbol == "A"):
+            return chr(ascii_number + 23)
+
+        return chr(ord(symbol) - 3)
+    else:
+        return symbol
+
 
 def encrypt_caesar(plaintext):
     ciphertext = ''.join(list(map(lambda x: inc_symbol(x), list(plaintext))))
     return ciphertext
 
-print(encrypt_caesar("Python3.6"))
+
+def decrypt_caesar(ciphertext):
+    plaintext = ''.join(list(map(lambda x: dec_symbol(x), list(ciphertext))))
+    return plaintext
+
+print(decrypt_caesar("Sawkrq3.6"))
