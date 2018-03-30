4323a4d51060d9c58aac21601ed5de4901d6e716
Alex Gumbin <lesha.gumbin@yandex.ru>
2018-03-30 12:51:28 +0300

    gcd function added

--- homework1/rsa.py
+++ homework1/rsa.py
@@ -27,8 +27,12 @@ def gcd(a, b):
     >>> gcd(3, 7)
     1
     """
-    # PUT YOUR CODE HERE
-    pass
+
+    while ((a % b) != 0):
+        c = a % b
+        a = b
+        b = c
+    return b
 
 
 def multiplicative_inverse(e, phi):
@@ -49,11 +53,9 @@ def generate_keypair(p, q):
     elif p == q:
         raise ValueError('p and q cannot be equal')
 
-    # n = pq
-    # PUT YOUR CODE HERE
+    n = p * q
 
-    # phi = (p-1)(q-1)
-    # PUT YOUR CODE HERE
+    phi = (p - 1) * (q - 1)
 
     # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
